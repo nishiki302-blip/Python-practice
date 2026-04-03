bc3aea101eda27020c32d76b99efe9c1fdce52a1
Nishiki <nishiki302@gmail.com>
2026-04-03 12:09:11 +0630

    Add comment

--- faces/faces.py
+++ faces/faces.py
@@ -1,9 +1,13 @@
+#request for user input
 say = input("Say: ")
 
 def main():
+    #use convert function and assign modify string to the new variable
     dsg_say = convert(say)
+    #print modify string
     print(dsg_say)
 
+#create convert function to change emoticons from user input
 def convert(to):
     return to.replace(':)','🙂').replace(':(','🙁')
 
