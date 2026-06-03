2e457ac89532aa8045a00617c169e64031856448
Nishiki <nishiki302@gmail.com>
2026-06-03 10:03:11 +0630

    add doc string

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -1,3 +1,10 @@
+"""
+Convert user's date that assume that user will input mm/dd/yy or 
+mm(eg-january) dd,yy format to yy-mm-dd format
+
+"""
+
+
 def main():
     month = [
         "January",
@@ -11,22 +18,22 @@ def main():
         "September",
         "October",
         "November",
-        "December"
+        "December",
     ]
     while True:
         try:
             date = input("Date: ")
-            if '/' in date:
-                m,d,y = date.split('/')
+            if "/" in date:
+                m, d, y = date.split("/")
                 m = int(m)
                 d = int(d)
                 y = int(y)
                 if d > 31 or m > 12:
                     raise ValueError()
                 print(f"{y:04}-{m:02}-{d:02}")
-            elif ',' in date:
-                md , y = date.split(',')
-                m,d = md.split()
+            elif "," in date:
+                md, y = date.split(",")
+                m, d = md.split()
                 d = int(d)
                 y = int(y)
                 if d > 31:
@@ -34,10 +41,12 @@ def main():
                 print(f"{y:04}-{(month.index(m) + 1):02}-{d:02}")
             else:
                 raise ValueError()
-            
+
         except ValueError:
             pass
 
         else:
             break
+
+
 main()
